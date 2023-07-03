cc7e638e1b3daa85f2eaedf433de93d21cf28a81
Ashfaque D <87939111+ashfaqued@users.noreply.github.com>
2023-07-03 21:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+ const processedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Reverse the processed string
+  const reversedStr = processedStr.split('').reverse().join('');
+  
+  // Compare the processed string with the reversed string
+  return processedStr === reversedStr;
 
 }
 module.exports = palindrome
